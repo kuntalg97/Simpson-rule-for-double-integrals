61fa7650ab51ec4028ab8809d2f0a3e86924cf99
kuntalg97 <kuntalghoshchem@gmail.com>
2022-05-28 16:57:50 -0500

    Modified for different number of grid points along X and Y

--- simpson_2d.f90
+++ simpson_2d.f90
@@ -10,7 +10,7 @@
       REAL*8 :: x1_start,x1_end,x2_start,x2_end,dx1,dx2,&
                 integ,x1,x2
       REAL*8, EXTERNAL :: expression
-      INTEGER*8 :: i,j,n
+      INTEGER*8 :: i,j,n,n1,n2
       INTEGER*8, ALLOCATABLE :: ss(:,:),sa(:),sb(:)
       REAL*8, ALLOCATABLE :: ff(:,:)
 
@@ -19,16 +19,18 @@
       x1_end = 1.0d0
       x2_start = 0.5d0
       x2_end = 1.0d0
-      n = 20000 !same no of grid points along x and y
+!      n = 20000
+      n1 = 2000
+      n2 = 3000
 
-      dx1 = (x1_end-x1_start)/DFLOAT(n-1)
-      dx2 = (x2_end-x2_start)/DFLOAT(n-1)
+      dx1 = (x1_end-x1_start)/DFLOAT(n1-1)
+      dx2 = (x2_end-x2_start)/DFLOAT(n2-1)
 
 !     Simpson's matrix is ss, function matrix is ff
 
-      ALLOCATE (ss(n,n),ff(n,n),sa(n),sb(n))
+      ALLOCATE (ss(n1,n2),ff(n1,n2),sa(n1),sb(n2))
 
-      DO i = 1,n
+      DO i = 1,n1
          IF (MOD(i,2)==0) THEN
             sa(i) = 4
          ELSE
@@ -37,26 +39,36 @@
       END DO
 
       sa(1) = 1
-      sa(n) = 1
-      sb(:) = sa(:)
+      sa(n1) = 1
 
-      DO i = 1,n
-         DO j = 1,n
+      DO i = 1,n2
+         IF (MOD(i,2)==0) THEN
+            sb(i) = 4
+         ELSE
+            sb(i) = 2
+         END IF
+      END DO
+
+      sb(1) = 1
+      sb(n2) = 1
+
+      DO i = 1,n1
+         DO j = 1,n2
             ss(i,j) = sa(i)*sb(j)
          END DO
       END DO
 
-      DO i = 1,n
+      DO i = 1,n1
          x1 = x1_start + (i-1)*dx1
-         DO j = 1,n
+         DO j = 1,n2
             x2 = x2_start + (j-1)*dx2
             ff(i,j) = expression(x1,x2)
          END DO
       END DO
 
       integ = 0.0d0
-      DO i = 1,n
-         DO j = 1,n
+      DO i = 1,n1
+         DO j = 1,n2
             integ = integ + ss(i,j)*ff(i,j)
          END DO
       END DO
